a24517811baaee2e6d4376fa6f23b6ee02e34bca
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-19 20:36:43 -0500

    feat: agregar getters, setter y métodos agregarCantidad y reiniciarCantidad

--- RestauranteBuenSabor/src/restaurante/Producto.java
+++ RestauranteBuenSabor/src/restaurante/Producto.java
@@ -12,6 +12,34 @@ public class Producto {
         this.precio   = precio;
         this.cantidad = 0;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        if (cantidad >= 0) {
+            this.cantidad = cantidad;
+        }
+    }
+
+    public void agregarCantidad(int unidades) {
+        if (unidades > 0) {
+            this.cantidad += unidades;
+        }
+    }
+
+    public void reiniciarCantidad() {
+        this.cantidad = 0;
+    }
    
 
 
