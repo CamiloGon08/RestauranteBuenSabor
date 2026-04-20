5347be856cc2eb61a8cbb1cb59dcba75cb690838
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-19 21:36:25 -0500

    refactor: agregar ejecutarMenuPrincipal(), mostrarOpciones() y procesarOpcion() eliminando main monolitico de 100 lineas

--- RestauranteBuenSabor/src/restaurante/RestauranteElBuenSabor.java
+++ RestauranteBuenSabor/src/restaurante/RestauranteElBuenSabor.java
@@ -13,5 +13,36 @@ public class RestauranteElBuenSabor {
         entrada.close();
     }
 
-    
+    private static void ejecutarMenuPrincipal() {
+        boolean activo = true;
+        while (activo) {
+            mostrarOpciones();
+            int opcion = leerEntero("Seleccione una opcion: ");
+            activo = procesarOpcion(opcion);
+        }
+    }
+
+    private static void mostrarOpciones() {
+        System.out.println("1. Ver carta");
+        System.out.println("2. Agregar producto al pedido");
+        System.out.println("3. Ver pedido actual");
+        System.out.println("4. Generar factura");
+        System.out.println("5. Nueva mesa");
+        System.out.println("0. Salir");
+        System.out.println(SEPARADOR);
+    }
+
+    private static boolean procesarOpcion(int opcion) {
+        switch (opcion) {
+            case 1  -> verCarta();
+            case 2  -> agregarProducto();
+            case 3  -> verPedido();
+            case 4  -> generarFactura();
+            case 5  -> nuevaMesa();
+            case 0  -> { System.out.println("Hasta luego!"); return false; }
+            default -> System.out.println("Opcion no valida. Seleccione entre 0 y 5.");
+        }
+        return true;
+    }
+
 }
