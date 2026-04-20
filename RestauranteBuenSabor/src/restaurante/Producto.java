53b8cf72da586a120e35322c85a41d83c90d875c
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-04-19 20:35:44 -0500

    feat: crear clase Producto con atributos nombre, precio y cantidad

--- RestauranteBuenSabor/src/restaurante/Producto.java
+++ RestauranteBuenSabor/src/restaurante/Producto.java
@@ -3,6 +3,15 @@
 
 public class Producto {
     
+    private final String nombre;
+    private final double precio;
+    private int cantidad;
+
+    public Producto(String nombre, double precio) {
+        this.nombre   = nombre;
+        this.precio   = precio;
+        this.cantidad = 0;
+    }
    
 
 
